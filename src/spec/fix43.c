ea453c1af4e8557408feb354d7e505205f05b2fd
CarpAIO <108148064+CarpAIO@users.noreply.github.com>
2022-09-04 15:27:28 +0000

    Update init.sqf

--- src/spec/fix43.c
+++ src/spec/fix43.c
@@ -3712,4 +3712,89 @@ static struct _ofixGroupSpec	EmailGroup627 = {
 	{ 628, false }, // HopCompID
 	{ 629, false }, // HopSendingTime
 	{ 630, false }, // HopRefID
-	{ 0, f
+	{ 0, false }
+    }
+};
+
+static struct _ofixGroupSpec	EmailGroup215 = {
+    215, // NoRoutingIDs
+    {
+	{ 216, false }, // RoutingType
+	{ 217, false }, // RoutingID
+	{ 0, false }
+    }
+};
+
+static struct _ofixGroupSpec	EmailGroup146 = {
+    146, // NoRelatedSym
+    {
+	{ 55, true }, // Symbol
+	{ 65, false }, // SymbolSfx
+	{ 48, false }, // SecurityID
+	{ 22, false }, // SecurityIDSource
+	{ 454, false }, // NoSecurityAltID
+	{ 460, false }, // Product
+	{ 461, false }, // CFICode
+	{ 167, false }, // SecurityType
+	{ 200, false }, // MaturityMonthYear
+	{ 541, false }, // MaturityDate
+	{ 224, false }, // CouponPaymentDate
+	{ 225, false }, // IssueDate
+	{ 239, false }, // RepoCollateralSecurityType
+	{ 226, false }, // RepurchaseTerm
+	{ 227, false }, // RepurchaseRate
+	{ 228, false }, // Factor
+	{ 255, false }, // CreditRating
+	{ 543, false }, // InstrRegistry
+	{ 470, false }, // CountryOfIssue
+	{ 471, false }, // StateOrProvinceOfIssue
+	{ 472, false }, // LocaleOfIssue
+	{ 240, false }, // RedemptionDate
+	{ 202, false }, // StrikePrice
+	{ 206, false }, // OptAttribute
+	{ 231, false }, // ContractMultiplier
+	{ 223, false }, // CouponRate
+	{ 207, false }, // SecurityExchange
+	{ 106, false }, // Issuer
+	{ 348, false }, // EncodedIssuerLen
+	{ 349, false }, // EncodedIssuer
+	{ 107, false }, // SecurityDesc
+	{ 350, false }, // EncodedSecurityDescLen
+	{ 351, false }, // EncodedSecurityDesc
+	{ 0, false }
+    }
+};
+
+static struct _ofixGroupSpec	EmailGroup454 = {
+    454, // NoSecurityAltID
+    {
+	{ 455, false }, // SecurityAltID
+	{ 456, false }, // SecurityAltIDSource
+	{ 0, false }
+    }
+};
+
+static struct _ofixGroupSpec	EmailGroup33 = {
+    33, // LinesOfText
+    {
+	{ 58, true }, // Text
+	{ 354, false }, // EncodedTextLen
+	{ 355, false }, // EncodedText
+	{ 0, false }
+    }
+};
+
+static ofixGroupSpec	EmailGroups[] = {
+    &EmailGroup627,
+    &EmailGroup215,
+    &EmailGroup146,
+    &EmailGroup454,
+    &EmailGroup33,
+    0
+};
+
+static struct _ofixMsgSpec	Email = {
+    &fix43Spec, // version
+    67, // tid
+    "C", // type
+    "Email", /
