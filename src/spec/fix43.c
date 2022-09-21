2d0f5fb7af59bacbaa9c381311c8e0883bbffde7
CarpAIO <108148064+CarpAIO@users.noreply.github.com>
2022-09-21 09:41:34 +0000

    Create main.py

--- src/spec/fix43.c
+++ src/spec/fix43.c
@@ -5248,4 +5248,70 @@ static struct _ofixMsgSpec	ListStatusRequest = {
 	{ 57, false }, // TargetSubID
 	{ 143, false }, // TargetLocationID
 	{ 116, false }, // OnBehalfOfSubID
-	{ 144, 
+	{ 144, false }, // OnBehalfOfLocationID
+	{ 129, false }, // DeliverToSubID
+	{ 145, false }, // DeliverToLocationID
+	{ 43, false }, // PossDupFlag
+	{ 97, false }, // PossResend
+	{ 52, true }, // SendingTime
+	{ 122, false }, // OrigSendingTime
+	{ 212, false }, // XmlDataLen
+	{ 213, false }, // XmlData
+	{ 347, false }, // MessageEncoding
+	{ 369, false }, // LastMsgSeqNumProcessed
+	{ 370, false }, // OnBehalfOfSendingTime
+	{ 627, false }, // NoHops
+	{ 66, true }, // ListID
+	{ 58, false }, // Text
+	{ 354, false }, // EncodedTextLen
+	{ 355, false }, // EncodedText
+	{ 93, false }, // SignatureLength
+	{ 89, false }, // Signature
+	{ 10, true }, // CheckSum
+	{ 0, false }
+    }
+};
+
+// ListStatus [N]
+
+static struct _ofixGroupSpec	ListStatusGroup627 = {
+    627, // NoHops
+    {
+	{ 628, false }, // HopCompID
+	{ 629, false }, // HopSendingTime
+	{ 630, false }, // HopRefID
+	{ 0, false }
+    }
+};
+
+static struct _ofixGroupSpec	ListStatusGroup73 = {
+    73, // NoOrders
+    {
+	{ 11, true }, // ClOrdID
+	{ 526, false }, // SecondaryClOrdID
+	{ 14, true }, // CumQty
+	{ 39, true }, // OrdStatus
+	{ 636, false }, // WorkingIndicator
+	{ 151, true }, // LeavesQty
+	{ 84, true }, // CxlQty
+	{ 6, true }, // AvgPx
+	{ 103, false }, // OrdRejReason
+	{ 58, false }, // Text
+	{ 354, false }, // EncodedTextLen
+	{ 355, false }, // EncodedText
+	{ 0, false }
+    }
+};
+
+static ofixGroupSpec	ListStatusGroups[] = {
+    &ListStatusGroup627,
+    &ListStatusGroup73,
+    0
+};
+
+static struct _ofixMsgSpec	ListStatus = {
+    &fix43Spec, // version
+    78, // tid
+    "N", // type
+    "ListStatus", // name
+    {0,0,0,0,0,0,0,0,1,2,42,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,3,0,0,0,0,0,0,0,19,0,0,0,0,0,4,11,0,21,0,0,0,5,13,0,0,37,0,0,0,0,0,29,0,38,0,0,0,0,39,0,0,0,0,0,0,0,0,31,33,0,0,0,0,0,41,8,9,0,40,0,0,0,20,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,15,0,0,0,0,0,22,0,0,0,0,0,7,17,0,0,0,0,0,0,0,0,0,0,0,0,12,14,16,18,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
