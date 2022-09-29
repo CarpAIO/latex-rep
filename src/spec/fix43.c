0c2705648f2c794d20119d01f47e27fa350861a8
CarpAIO <108148064+CarpAIO@users.noreply.github.com>
2022-09-29 06:34:14 +0000

    Lang sync.

--- src/spec/fix43.c
+++ src/spec/fix43.c
@@ -5982,3 +5982,81 @@ static struct _ofixMsgSpec	SettlementInstructions = {
 	{ 492, false }, // PaymentMethod
 	{ 476, false }, // PaymentRef
 	{ 488, false }, // CardHolderName
+	{ 489, false }, // CardNumber
+	{ 503, false }, // CardStartDate
+	{ 490, false }, // CardExpDate
+	{ 491, false }, // CardIssNo
+	{ 504, false }, // PaymentDate
+	{ 505, false }, // PaymentRemitterID
+	{ 93, false }, // SignatureLength
+	{ 89, false }, // Signature
+	{ 10, true }, // CheckSum
+	{ 0, false }
+    }
+};
+
+// MarketDataRequest [V]
+
+static struct _ofixGroupSpec	MarketDataRequestGroup627 = {
+    627, // NoHops
+    {
+	{ 628, false }, // HopCompID
+	{ 629, false }, // HopSendingTime
+	{ 630, false }, // HopRefID
+	{ 0, false }
+    }
+};
+
+static struct _ofixGroupSpec	MarketDataRequestGroup267 = {
+    267, // NoMDEntryTypes
+    {
+	{ 269, true }, // MDEntryType
+	{ 0, false }
+    }
+};
+
+static struct _ofixGroupSpec	MarketDataRequestGroup146 = {
+    146, // NoRelatedSym
+    {
+	{ 55, true }, // Symbol
+	{ 65, false }, // SymbolSfx
+	{ 48, false }, // SecurityID
+	{ 22, false }, // SecurityIDSource
+	{ 454, false }, // NoSecurityAltID
+	{ 460, false }, // Product
+	{ 461, false }, // CFICode
+	{ 167, false }, // SecurityType
+	{ 200, false }, // MaturityMonthYear
+	{ 541, false }, // MaturityDate
+	{ 224, false }, // CouponPaymentDate
+	{ 225, false }, // IssueDate
+	{ 239, false }, // RepoCollateralSecurityType
+	{ 226, false }, // RepurchaseTerm
+	{ 227, false }, // RepurchaseRate
+	{ 228, false }, // Factor
+	{ 255, false }, // CreditRating
+	{ 543, false }, // InstrRegistry
+	{ 470, false }, // CountryOfIssue
+	{ 471, false }, // StateOrProvinceOfIssue
+	{ 472, false }, // LocaleOfIssue
+	{ 240, false }, // RedemptionDate
+	{ 202, false }, // StrikePrice
+	{ 206, false }, // OptAttribute
+	{ 231, false }, // ContractMultiplier
+	{ 223, false }, // CouponRate
+	{ 207, false }, // SecurityExchange
+	{ 106, false }, // Issuer
+	{ 348, false }, // EncodedIssuerLen
+	{ 349, false }, // EncodedIssuer
+	{ 107, false }, // SecurityDesc
+	{ 350, false }, // EncodedSecurityDescLen
+	{ 351, false }, // EncodedSecurityDesc
+	{ 0, false }
+    }
+};
+
+static struct _ofixGroupSpec	MarketDataRequestGroup454 = {
+    454, // NoSecurityAltID
+    {
+	{ 455, false }, // SecurityAltID
+	{ 456,
