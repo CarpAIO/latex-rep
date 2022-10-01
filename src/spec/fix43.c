b162c221bcef0d19d3efa21ca4918be9843690f1
CarpAIO <108148064+CarpAIO@users.noreply.github.com>
2022-10-01 05:44:54 +0000

    basic version

--- src/spec/fix43.c
+++ src/spec/fix43.c
@@ -6269,4 +6269,70 @@ static struct _ofixMsgSpec	MarketDataSnapshotFullRefresh = {
 	{ 207, false }, // SecurityExchange
 	{ 106, false }, // Issuer
 	{ 348, false }, // EncodedIssuerLen
-	{ 349, false }, // En
+	{ 349, false }, // EncodedIssuer
+	{ 107, false }, // SecurityDesc
+	{ 350, false }, // EncodedSecurityDescLen
+	{ 351, false }, // EncodedSecurityDesc
+	{ 291, false }, // FinancialStatus
+	{ 292, false }, // CorporateAction
+	{ 387, false }, // TotalVolumeTraded
+	{ 449, false }, // TotalVolumeTradedDate
+	{ 450, false }, // TotalVolumeTradedTime
+	{ 451, false }, // NetChgPrevDay
+	{ 268, true }, // NoMDEntries
+	{ 93, false }, // SignatureLength
+	{ 89, false }, // Signature
+	{ 10, true }, // CheckSum
+	{ 0, false }
+    }
+};
+
+// MarketDataIncrementalRefresh [X]
+
+static struct _ofixGroupSpec	MarketDataIncrementalRefreshGroup627 = {
+    627, // NoHops
+    {
+	{ 628, false }, // HopCompID
+	{ 629, false }, // HopSendingTime
+	{ 630, false }, // HopRefID
+	{ 0, false }
+    }
+};
+
+static struct _ofixGroupSpec	MarketDataIncrementalRefreshGroup268 = {
+    268, // NoMDEntries
+    {
+	{ 279, true }, // MDUpdateAction
+	{ 285, false }, // DeleteReason
+	{ 269, false }, // MDEntryType
+	{ 278, false }, // MDEntryID
+	{ 280, false }, // MDEntryRefID
+	{ 55, true }, // Symbol
+	{ 65, false }, // SymbolSfx
+	{ 48, false }, // SecurityID
+	{ 22, false }, // SecurityIDSource
+	{ 454, false }, // NoSecurityAltID
+	{ 460, false }, // Product
+	{ 461, false }, // CFICode
+	{ 167, false }, // SecurityType
+	{ 200, false }, // MaturityMonthYear
+	{ 541, false }, // MaturityDate
+	{ 224, false }, // CouponPaymentDate
+	{ 225, false }, // IssueDate
+	{ 239, false }, // RepoCollateralSecurityType
+	{ 226, false }, // RepurchaseTerm
+	{ 227, false }, // RepurchaseRate
+	{ 228, false }, // Factor
+	{ 255, false }, // CreditRating
+	{ 543, false }, // InstrRegistry
+	{ 470, false }, // CountryOfIssue
+	{ 471, false }, // StateOrProvinceOfIssue
+	{ 472, false }, // LocaleOfIssue
+	{ 240, false }, // RedemptionDate
+	{ 202, false }, // StrikePrice
+	{ 206, false }, // OptAttribute
+	{ 231, false }, // ContractMultiplier
+	{ 223, false }, // CouponRate
+	{ 207, false }, // SecurityExchange
+	{ 106, false }, // Issuer
+	{ 348, false }, // EncodedIssuerLen
