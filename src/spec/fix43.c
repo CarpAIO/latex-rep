2478c831be36197c904e56b6983a00960e2910d5
CarpAIO <108148064+CarpAIO@users.noreply.github.com>
2022-12-22 19:24:16 +0000

    2.7.7

--- src/spec/fix43.c
+++ src/spec/fix43.c
@@ -9819,4 +9819,74 @@ static struct _ofixGroupSpec	SecurityListGroup454 = {
     454, // NoSecurityAltID
     {
 	{ 455, false }, // SecurityAltID
-	{ 456, false }, //
+	{ 456, false }, // SecurityAltIDSource
+	{ 0, false }
+    }
+};
+
+static struct _ofixGroupSpec	SecurityListGroup555 = {
+    555, // NoLegs
+    {
+	{ 600, true }, // LegSymbol
+	{ 601, false }, // LegSymbolSfx
+	{ 602, false }, // LegSecurityID
+	{ 603, false }, // LegSecurityIDSource
+	{ 604, false }, // NoLegSecurityAltID
+	{ 607, false }, // LegProduct
+	{ 608, false }, // LegCFICode
+	{ 609, false }, // LegSecurityType
+	{ 610, false }, // LegMaturityMonthYear
+	{ 611, false }, // LegMaturityDate
+	{ 248, false }, // LegCouponPaymentDate
+	{ 249, false }, // LegIssueDate
+	{ 250, false }, // LegRepoCollateralSecurityType
+	{ 251, false }, // LegRepurchaseTerm
+	{ 252, false }, // LegRepurchaseRate
+	{ 253, false }, // LegFactor
+	{ 257, false }, // LegCreditRating
+	{ 599, false }, // LegInstrRegistry
+	{ 596, false }, // LegCountryOfIssue
+	{ 597, false }, // LegStateOrProvinceOfIssue
+	{ 598, false }, // LegLocaleOfIssue
+	{ 254, false }, // LegRedemptionDate
+	{ 612, false }, // LegStrikePrice
+	{ 613, false }, // LegOptAttribute
+	{ 614, false }, // LegContractMultiplier
+	{ 615, false }, // LegCouponRate
+	{ 616, false }, // LegSecurityExchange
+	{ 617, false }, // LegIssuer
+	{ 618, false }, // EncodedLegIssuerLen
+	{ 619, false }, // EncodedLegIssuer
+	{ 620, false }, // LegSecurityDesc
+	{ 621, false }, // EncodedLegSecurityDescLen
+	{ 622, false }, // EncodedLegSecurityDesc
+	{ 623, false }, // LegRatioQty
+	{ 624, false }, // LegSide
+	{ 556, false }, // LegCurrency
+	{ 0, false }
+    }
+};
+
+static struct _ofixGroupSpec	SecurityListGroup604 = {
+    604, // NoLegSecurityAltID
+    {
+	{ 605, false }, // LegSecurityAltID
+	{ 606, false }, // LegSecurityAltIDSource
+	{ 0, false }
+    }
+};
+
+static ofixGroupSpec	SecurityListGroups[] = {
+    &SecurityListGroup627,
+    &SecurityListGroup146,
+    &SecurityListGroup454,
+    &SecurityListGroup555,
+    &SecurityListGroup604,
+    0
+};
+
+static struct _ofixMsgSpec	SecurityList = {
+    &fix43Spec, // version
+    121, // tid
+    "y", // type
+    "SecurityList", // n
