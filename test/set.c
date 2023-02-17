707413da5bbeb673373071e88a8a0be30a282cf7
CarpAIO <108148064+CarpAIO@users.noreply.github.com>
2023-02-17 19:43:08 +0000

    PM: devfreq: Fix simple_ondemand crashing on startup

--- test/set.c
+++ test/set.c
@@ -302,4 +302,73 @@ NULL or zero length string is not a valid value for ofix_msg_set_str().\n";
 		test_fail();
 		ofix_err_clear(&err);
 	    }
-	    str = ofix_msg_to_str(&err, msg
+	    str = ofix_msg_to_str(&err, msg);
+	    if (OFIX_OK != err.code) {
+		test_print("[%d] %s\n", err.code, err.msg);
+		test_fail();
+	    }
+	    a += sprintf(a, "%s\n", str);
+	    free(str);
+	}
+    }
+    ofix_msg_destroy(msg);
+    test_same(expected, actual);
+}
+
+static void
+set_timestamp_test() {
+    char		actual[1024];
+    char		*a = actual;
+    char		*expected = "\
+8=FIX.4.4^9=028^35=A^5000=20071130-19:44:33^10=131^\n\
+8=FIX.4.4^9=032^35=A^5000=20080101-23:59:59.999^10=094^\n\
+8=FIX.4.4^9=032^35=A^5000=00000101-00:00:00.000^10=024^\n\
+8=FIX.4.4^9=032^35=A^5000=99991231-23:59:59.999^10=125^\n";
+    struct _ofixErr	err = OFIX_ERR_INIT;
+    ofixMsg		msg;
+    struct _ofixDate	values[] = {
+	{ 2007, 11, 0, 30, 19, 44, 33, 0, OFIX_TIMESTAMP },
+	{ 2008, 1, 0, 1, 23, 59, 59, 999, OFIX_TIMESTAMP },
+	{ 0, 1, 0, 1, 0, 0, 0, 0, OFIX_TIMESTAMP },
+	{ 9999, 12, 0, 31, 23, 59, 59, 999, OFIX_TIMESTAMP } };
+    ofixDate		vp;
+    ofixDate		end = (ofixDate)((char*)values + sizeof(values));
+    char		*str;
+
+    if (NULL == (msg = ofix_msg_create(&err, "A", 4, 4, 14))) {
+	test_print("[%d] %s\n", err.code, err.msg);
+	test_fail();
+	return;
+    }
+    for (vp = values; vp < end; vp++) {
+	ofix_msg_set_date(&err, msg, 5000, vp);
+	if (OFIX_OK != err.code) {
+	    test_print("set date of %d failed: [%d] %s\n", err.code, err.msg);
+	    test_fail();
+	    ofix_err_clear(&err);
+	}
+	str = ofix_msg_to_str(&err, msg);
+	if (OFIX_OK != err.code) {
+	    test_print("[%d] %s\n", err.code, err.msg);
+	    test_fail();
+	}
+	a += sprintf(a, "%s\n", str);
+	free(str);
+    }
+    ofix_msg_destroy(msg);
+    test_same(expected, actual);
+}
+
+static void
+set_timeonly_test() {
+    char		actual[1024];
+    char		*a = actual;
+    char		*expected = "\
+8=FIX.4.4^9=019^35=A^5000=19:44:33^10=200^\n\
+8=FIX.4.4^9=023^35=A^5000=23:59:59.999^10=165^\n\
+8=FIX.4.4^9=023^35=A^5000=00:00:00.000^10=105^\n";
+    struct _ofixErr	err = OFIX_ERR_INIT;
+    ofixMsg		msg;
+    struct _ofixDate	values[] = {
+	{ 0, 0, 0, 0, 19, 44, 33, 0, OFIX_TIMEONLY },
+	
